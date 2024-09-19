e295212e88d10b20b5463ce5885776e825576ec9
ellandri <ahou.405@gmail.com>
2024-09-19 08:22:35 +0000

    first fonction

--- src/books/viewsets/reservation_views.py
+++ src/books/viewsets/reservation_views.py
@@ -8,3 +8,27 @@ from books.models.reservation_model import ReservationModel
 class ReservationViewset(viewsets.ModelViewSet):
     serializer_class = ReservationSerializer
     queryset = ReservationModel.objects.all()
+    
+    @csrf_exempt
+    def reservation_detail(request, id):
+        try :
+            reservation = ReservationModel.objects.get(id=id)
+        except ReservationModel.DoesNotExist : 
+            return HttpResponse(status=404)
+        
+        if request.method == 'GET':
+            serializer =  ReservationSerializer(reservation)
+            return JsonResponse(serializer.data)
+    
+    
+    @csrf_exempt
+    def reservation_detail_by_user(request, id):
+        try :
+            reservation = ReservationModel.objects.filter(user = id)
+        except ReservationModel.DoesNotExist : 
+            return HttpResponse(status=404)
+        
+        
+        if request.method == 'GET':
+            serializer =  ReservationSerializer(reservation,many=True)
+            return JsonResponse(serializer.data, safe=False)
